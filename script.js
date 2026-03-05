3e7759a00665ade63afe970c63361a45d40d1b88
arslan <244682724+justCallMeArslan@users.noreply.github.com>
2026-03-05 17:08:13 +0300

    Need test and move to DOM

--- script.js
+++ script.js
@@ -47,7 +47,24 @@ function Gameboard() {
     return { placeMark, getGrid, resetBoard, isFull }; // returns for usage in gameController
 }
 
-function winChecker(boardInPlay) {
+function Player(mark, nickname) {
+
+    //adding validation for nickname
+    const isValid = /^[a-zA-Z]+$/.test(nickname); //returns true/false
+    function getMark() { // closure for returning mark of player
+        return mark
+    }
+    function getNickname() { // closure for returning name of player
+        return nickname
+    }
+
+    function getPlayerCard() {
+        return { mark, nickname }
+    }
+    return { getMark, getNickname, getPlayerCard, isValid };
+}
+
+function roundWin(boardInPlay) {
     const winningLines = [
         //rows 
         [0, 1, 2],
@@ -67,11 +84,117 @@ function winChecker(boardInPlay) {
     for (const [a, b, c] of winningLines) {
         // comparing a, b, c that they are not empty and the same X or O
         if (boardInPlay[a] && boardInPlay[a] === boardInPlay[b] && boardInPlay[a] === boardInPlay[c]) {
-            return boardInPlay[a]; // return X or O as a mark of winner
+            return boardInPlay[a]; // return X or O as a mark of round winner
         }
     }
-    return null; // if no winner on the board 
+    return null; // if no round winner on the board 
 }
+
+
+function gameController() {
+    const board = Gameboard();
+
+    const playerOne = Player("X", "DOMforX"); // nickname is going to be changed by DOM 
+    const playerTwo = Player("O", "DOMforO"); // nickname is going to be changed by DOM 
+    if (!playerOne.isValid || !playerTwo.isValid) {
+        return {
+            type: "INVALID_PLAYER",
+            scope: {
+                reason: "Only latin alphabet allowed",
+            }
+        }
+    }
+    const players = [playerOne, playerTwo];
+    let currentPlayer = players[0];
+
+    function switchPlayers() {
+        currentPlayer = currentPlayer === players[0] ? players[1] : players[0] // checks  
+        // what is player now (default is players[0]) and switches to opposite
+    }
+
+    function getCurrentPlayer() { // getter of who is CP
+        return currentPlayer;
+    }
+
+    // tracker of win counts (rounds)
+    let scores = {
+        X: 0,
+        O: 0,
+    }
+
+    let gameOver = false; // flag when game should or shouldn't allow makeMove
+
+    // attmempting to place mark,
+    function makeMove(index) {
+        if (gameOver === true) {
+            return {
+                type: "GAME_OVER"
+            }
+        }
+
+        //placing mark until success
+        const result = board.placeMark(index, currentPlayer.getMark());
+        if (result.status === false) {
+            return result;
+        }
+
+        // checking for winner
+        const roundWinner = roundWin(board.getGrid())
+        if (roundWinner !== null) {
+            scores[roundWinner] += 1;
+            matchWinner();
+            resetRound();
+            gameOver = true;
+            return {
+                type: "WIN",
+                scope: {
+                    mark: roundWinner,
+                    nickname: currentPlayer.getNickname()
+                }
+            };
+        } else if ((board.isFull())) {
+            gameOver = true
+            return {
+                type: "TIE"
+            }
+        } else {
+            switchPlayers();
+            return {
+                type: "PLAYING",
+                scope: { nextPlayer: currentPlayer.getNickname() }
+            }
+        }
+    }
+
+    function matchWinner() {
+        if (scores[playerOne.getMark()] === 3) {
+            return playerOne.getNickname() + 'wins the game';
+        } else if (scores[playerTwo.getMark()] === 3) {
+            return playerTwo.getNickname() + 'wins the game';
+        }
+        return null;
+    }
+
+
+    function resetRound() { // reset function after round
+        board.resetBoard();
+        gameOver = false;
+        currentPlayer = players[0];
+    }
+
+    function resetGame() { // staged for UI game reset (probably merger New Game button functionality)
+        board.resetBoard();
+        gameOver = false;
+        currentPlayer = players[0];
+
+        scores.X = 0;
+        scores.O = 0;
+    }
+
+
+    return { getCurrentPlayer, makeMove, resetGame }
+}
+
 
 // test of Gameboard() functionallity
 // const board = Gameboard();
@@ -91,94 +214,3 @@ function winChecker(boardInPlay) {
 // // board.resetBoard(); // board is empty
 
 
-
-
-function Player(mark, nickname) {
-
-    //adding validation for nickname
-    const isValid = /^[a-zA-Z]+$/.test(nickname); //returns true/false
-    function getMark() { // closure for returning mark of player
-        return mark
-    }
-    function getNickname() { // closure for returning name of player
-        return nickname
-    }
-    return { getMark, getNickname, isValid };
-}
-
-
-
-
-function gameController() {
-    const board = Gameboard();
-    const playerOne = Player("X", "DOMforX"); // nickname is going to be changed by DOM 
-    const playerTwo = Player("O", "DOMforO"); // nickname is going to be changed by DOM 
-    if (!playerOne.isValid || !playerTwo.isValid) {
-        return {
-            type: "INVALID_MOVE",
-            scope: {
-                reason: "Only latin alphabet allowed",
-            }
-        }
-    }
-
-    const players = [playerOne, playerTwo];
-    let currentPlayer = players[0];
-
-    function switchPlayers() {
-        currentPlayer = currentPlayer === players[0] ? players[1] : players[0] // checks  
-        // what is player now (default is players[0]) and switches to opposite
-    }
-
-    function getCurrentPlayer() { // getter of who is CP
-        return currentPlayer;
-    }
-    let gameOver = false; // flag when game should or shouldn't allow makeMove
-
-    // attmempting to place mark,
-    function makeMove(index) {
-        if (gameOver === true) {
-            return {
-                type: "GAME_OVER"
-            }
-        }
-        const result = board.placeMark(index, currentPlayer.getMark());
-        if (result.status === false) {
-            return result;
-        }
-
-        // checking for winner
-        const winner = winChecker(board.getGrid())
-        if (winner !== null) {
-            gameOver = true;
-            return {
-                type: "WIN",
-                scope: {
-                    mark: winner,
-                    nickname: currentPlayer.getNickname()
-                }
-
-            };
-        } else if ((board.isFull())) {
-            gameOver = true
-            return {
-                type: "TIE"
-            }
-        } else {
-            switchPlayers();
-            return {
-                type: "PLAYING",
-                scope: { nextPlayer: currentPlayer.getNickname() }
-            }
-
-        }
-    }
-    function resetGame() { // reset function for UI implementation
-        board.resetBoard();
-        gameOver = false;
-        currentPlayer = players[0];
-    }
-
-    return { getCurrentPlayer, makeMove, resetGame }
-
-}
